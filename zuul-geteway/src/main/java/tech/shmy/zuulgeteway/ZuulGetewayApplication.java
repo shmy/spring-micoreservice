1cd79c553a66db609e0a1c5323a7892ccadb648e
shmy <pandahao123>
2020-11-23 20:15:23 +0800

    first commit

---
+++ zuul-geteway/src/main/java/tech/shmy/zuulgeteway/ZuulGetewayApplication.java
@@ -0,0 +1,17 @@
+package tech.shmy.zuulgeteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulGetewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGetewayApplication.class, args);
+	}
+
+}
---
+++ zuul-geteway/src/test/java/tech/shmy/zuulgeteway/ZuulGetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.shmy.zuulgeteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGetewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
