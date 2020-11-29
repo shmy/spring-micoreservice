2afa57dcff5cbfd5475f69b64529e5d806407f08
shmy <pandahao123>
2020-11-29 10:32:18 +0800

    add Scheduled

--- oss-server/src/main/java/tech/shmy/ossserver/OssServerApplication.java
+++ oss-server/src/main/java/tech/shmy/ossserver/OssServerApplication.java
@@ -3,9 +3,11 @@ package tech.shmy.ossserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableScheduling
 public class OssServerApplication {
 
 	public static void main(String[] args) {
